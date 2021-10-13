a58ed35bbd8ad9a7fad4829bed56d95706044349
ivanvrdev <ivangaston20032017@gmail.com>
2021-10-13 16:54:17 -0300

    reorganización de la página del TaskCRUD 13-10-2021

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import TasksCRUD from "./pages/TasksCRUD";
 function App() {
   return (
     <>
-      <h1 className="text-center">Tasks management</h1>
-      <div className="container">
-        <div className="row my-1">
-          <div className="col">
-            <TasksCRUD/>
-          </div>
-        </div>
-      </div>
+      <TasksCRUD/>
     </>
   );
 }
--- src/pages/TasksCRUD.jsx
+++ src/pages/TasksCRUD.jsx
@@ -3,35 +3,41 @@ import {DeleteTask, UpdateTask, CreateTask, ReadTasks} from '../components/Butto
 
 const TasksCRUD = () => {
     return (
-        <div>
-            <CreateTask/>
-            <ReadTasks/>
-            <div className="table-responsive">
-                <table className="table">
-                <thead>
-                    <tr>
-                    <th scope="col">Number</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Designed</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Create a CRUD with React library for JavaScript</td>
-                    <td>Iván</td>
-                    <td><span className="badge rounded-pill bg-success">Done</span></td>
-                    <td>
-                        <UpdateTask/>
-                        <DeleteTask/>
-                    </td>
-                    </tr>
-                </tbody>
-                </table>
+        <>
+            <h1 className="text-center">Tasks management</h1>
+            <div className="container">
+                <CreateTask/>
+                <ReadTasks/>
+                <div className="row my-1">
+                    <div className="col"></div>
+                        <div className="table-responsive">
+                            <table className="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">Number</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Designed</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Create a CRUD with React library for JavaScript</td>
+                                <td>Iván</td>
+                                <td><span className="badge rounded-pill bg-success">Done</span></td>
+                                <td>
+                                    <UpdateTask/>
+                                    <DeleteTask/>
+                                </td>
+                                </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
