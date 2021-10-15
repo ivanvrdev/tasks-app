360d882b1bbe64086951a5054f017f0e0200c68d
ivanvrdev <ivangaston20032017@gmail.com>
2021-10-15 16:04:59 -0300

    Test useState 15-10-2021

--- src/pages/TasksCRUD.jsx
+++ src/pages/TasksCRUD.jsx
@@ -1,40 +1,79 @@
-import React from 'react'
-import {DeleteTask, UpdateTask, CreateTask, ReadTasks} from '../components/Buttons'
+import React, {useState} from 'react'
 
 const TasksCRUD = () => {
+
+    const [showTable, setShowTable] = useState(true)
+
+    const CreateTaskForm = () => {
+        return (
+            <div>
+                <h1>Form</h1>
+                <input className="btn btn-danger my-1 mx-1"
+                    onClick={()=>setShowTable(true)}
+                    type="button"
+                    value="Cancel"
+                />
+            </div>
+        )
+    }
+
+    const TasksTable = () => {
+        return (
+            <div className="table-responsive">
+                <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Number</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Designed</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Create a CRUD with React library for JavaScript</td>
+                    <td>Iván</td>
+                    <td><span className="badge rounded-pill bg-success">Done</span></td>
+                    <td>
+                        <input className="btn btn-warning mx-1"
+                            onClick={()=>setShowTable(false)}
+                            type="button"
+                            value="Update"
+                        />
+                        <input className="btn btn-danger mx-1"
+                            onClick={()=>setShowTable(false)}
+                            type="button"
+                            value="Delete"
+                        />
+                    </td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    const CreateButton = ()=>{
+        return(
+            <input className="btn btn-success my-1 mx-1"
+                onClick={()=>setShowTable(false)}
+                type="button"
+                value="Create"
+            />
+        )
+    }
+
     return (
         <>
             <h1 className="text-center">Tasks management</h1>
             <div className="container">
-                <CreateTask/>
-                <ReadTasks/>
+                {showTable ? <CreateButton/>  : null}
                 <div className="row my-1">
-                    <div className="col"></div>
-                        <div className="table-responsive">
-                            <table className="table">
-                            <thead>
-                                <tr>
-                                <th scope="col">Number</th>
-                                <th scope="col">Description</th>
-                                <th scope="col">Designed</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <th scope="row">1</th>
-                                <td>Create a CRUD with React library for JavaScript</td>
-                                <td>Iván</td>
-                                <td><span className="badge rounded-pill bg-success">Done</span></td>
-                                <td>
-                                    <UpdateTask/>
-                                    <DeleteTask/>
-                                </td>
-                                </tr>
-                            </tbody>
-                            </table>
-                        </div>
+                    <div className="col">
+                        {showTable ? <TasksTable/> : <CreateTaskForm/>}
+                    </div>
                 </div>
             </div>
         </>
